2f221c8b9460bc6ef1ef74fdc2fb6eee5c7d2b26
Slavko15 <15slavko@gmail.com>
2024-01-07 13:47:48 +0200

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,61 +1,17 @@
-$("iframe").each(
-     function(index, elem) {
-         elem.setAttribute("scrolling","no");
-         elem.setAttribute("frameBorder","0");
-         elem.setAttribute("width","100%");
-         elem.setAttribute("height","100%");
-        elem.setAttribute("margin", "0 auto");
-     }
- );  
+document.addEventListener("DOMContentLoaded", function() {
+    var loaderVideo = document.getElementById('loaderVideo');
 
+    // Simulate a loading process
+    setTimeout(function() {
+        // Randomly decide if loading was a success or error
+        var isSuccess = Math.random() > 2;
 
-$('.grid').isotope({
-  transitionDuration: 0
-});
+        if (isSuccess) {
+            loaderVideo.src = 'path/to/success.webm';
+        } else {
+            loaderVideo.src = 'path/to/error.webm';
+        }
 
-$(document).ready(function(){
-  // init Isotope
-  var $grid = $('.grid').isotope({
-    itemSelector: '.item'
-  });
-
-  // store filter for each group
-  var filters = {};
-
-  $('.filters').on('change', '.filters-select', function(){
-    var $this = $(this);
-    //var selectGroup = $this.parents('.button-group');
-    var filterGroup = $this.attr('data-filter-group');
-
-    filters[ filterGroup ] = $this.val();
-
-    var filterValue = concatValues( filters );
-
-    $grid.isotope({ filter: filterValue });
-
-    console.log(filterGroup, filterValue);
-  });
-  
-  // call function to set default val
-  setDefault();
-    //----------------------------------------------------------------------
-  // HELP FROM a USER who is not registered at CSS-TRICKS.COM
-  // use this function, or use the code in the function where uyou need it
-  //----------------------------------------------------------------------
- 
-  function setDefault() {
-  	$('option[value=".blue"]').attr('selected', true); 
-    $('option[value=".large"]').attr('selected', true); // select yellow
-		$('.filters-select').trigger("change");							 // trigger change
-	}
-  //----------------------------------------------------------------------
-
-  // flatten object by concatting values
-  function concatValues( obj ) {
-    var value = '';
-    for ( var prop in obj ) {
-      value += obj[ prop ];
-    }
-    return value;
-  }
+        loaderVideo.loop = false; // Stop looping when showing success/error
+    }, 3000); // 3 seconds loading simulation
 });
